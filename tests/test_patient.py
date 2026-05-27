60a5657fa9b8c5a6e388523b8ba0c6ba87b0ce1a
BramJoseph <b.j.jansen@tudelft.nl>
2026-05-27 11:13:32 +0200

    Making a class for data

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,10 +1,36 @@
 """Tests for the Patient model."""
 
+
 from inflammation.models import Patient
+
+import numpy.testing as npt
 
 def test_create_patient():
 
     name = 'Alice'
-    p = Patient(name=name)
+    weight = 50
+    height = 1.8
+    
+    p = Patient(name = name, weight = weight, height = height)
 
     assert p.name == name
+    assert p.weight == weight
+    assert p.height == height
+
+
+def test_compute_patient_bmi():
+    
+    name = 'Maria'
+    weight = 60
+    height = 1.6
+
+    Maria = Patient(name = name, weight = weight, height = height)
+    expected_bmi = 23.4375
+    bmi = Maria.get_body_mass_index() 
+    
+    npt.assert_almost_equal(bmi, expected_bmi)
+
+
+
+
+
